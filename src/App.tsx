3b19d6bcb4ee9c100d1aac171bf6093c3b3a6dc8
aalamkh <aalam_khan96@outlook.com>
2023-12-19 10:26:38 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Routes, Route } from 'react-router-dom';
+import './globals.css';
+import { AllUsers, CreatePost, EditPost, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/Pages';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Toaster } from "@/components/ui/toaster";
+const App = () => {
+    return (
+        <main className='flex h-screen'>
+            <Routes>
+                {/*public Routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/sign-in" element={<SigninForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
+                </Route>
+
+                {/*private Routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/saved' element={<Saved />} />
+                    <Route path='/all-users' element={<AllUsers />} />
+                    <Route path='/create-post' element={<CreatePost />} />
+                    <Route path='/update-post/:id' element={<EditPost />} />
+                    <Route path='/posts/:id' element={<PostDetails />} />
+                    <Route path='/profile/:id/*' element={<Profile />} />
+                    <Route path='/update-profile/:id' element={<UpdateProfile />} />
+                </Route>
+            </Routes>
+            <Toaster />
+        </main>
+    )
+}
+
+export default App
